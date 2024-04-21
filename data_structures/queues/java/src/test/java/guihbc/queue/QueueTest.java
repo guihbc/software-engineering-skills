4258abf056397f24a4c1867bdab1a7e651e11580
guihcodes <guihchavesb@gmail.com>
2024-04-21 20:30:58 -0300

    add a break line

--- data_structures/queues/java/src/test/java/guihbc/queue/QueueTest.java
+++ data_structures/queues/java/src/test/java/guihbc/queue/QueueTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class QueueTest {
+    
     @Test
     public void shouldInsertInTheQueue() {
         Queue queue = new Queue(1);
